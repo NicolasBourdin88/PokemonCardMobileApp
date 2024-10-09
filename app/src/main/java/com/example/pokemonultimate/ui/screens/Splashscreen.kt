f2e0c63bed939526e311f0f672a9b513db001999
Sullivan Bittiger <sullivan9025@gmail.com>
2024-10-09 17:09:38 +0200

    add connection with firebase email password

--- app/src/main/java/com/example/pokemonultimate/ui/screens/Splashscreen.kt
+++ app/src/main/java/com/example/pokemonultimate/ui/screens/Splashscreen.kt
@@ -26,7 +26,7 @@ import com.example.pokemonultimate.MainActivity
 import com.example.pokemonultimate.R
 import kotlinx.coroutines.delay
 
-const val SPLASH_SCREEN_DURATION: Long = 2000L
+const val SPLASH_SCREEN_DURATION: Long = 0L
 
 @Composable
 fun SplashScreen(navController: NavController) {
