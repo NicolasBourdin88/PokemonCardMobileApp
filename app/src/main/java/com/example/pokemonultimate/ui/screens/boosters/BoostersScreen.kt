a354295a16207609bf80d426b76d6ef83574fbd5
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2024-10-18 15:15:09 +0200

    Animation boosters

--- app/src/main/java/com/example/pokemonultimate/ui/screens/boosters/BoostersScreen.kt
+++ app/src/main/java/com/example/pokemonultimate/ui/screens/boosters/BoostersScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -28,13 +31,92 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pokemonultimate.R
 import com.example.pokemonultimate.ui.utils.TitleText
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
 fun BoostersScreen() {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         TitleText("Want To Open A Booster ?")
-        Booster(height = 350, width = 200)
+        Booster2(height = 450, width = 200)
+    }
+}
+
+@Preview
+@Composable
+fun Booster2(height: Int = 450, width: Int = 200) {
+    val swipeThreshold = 100f
+    var isSwipe: Boolean by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+                detectHorizontalDragGestures { change, dragAmount ->
+                    change.consume()
+                    if (dragAmount > swipeThreshold || dragAmount < -swipeThreshold) {
+                        isSwipe = true
+                    }
+                }
+            }, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            Modifier
+                .height(height.dp)
+                .width(width.dp)
+        ) {
+            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+                BottomBooster(height, width)
+            }
+            TopBooster(isSwipe)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BottomBooster(height: Int = 450, width: Int = 200) {
+    Column {
+        Quadrilateral(
+            modifier = Modifier
+                .height((height - 75).dp)
+                .width(width.dp)
+                .padding(horizontal = 4.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.piece_of_boosters),
+            contentDescription = "Piece in aluminium of booster",
+            modifier = Modifier
+                .fillMaxWidth()
+                .rotate(180F),
+            contentScale = ContentScale.FillWidth
+        )
+    }
+}
+
+@Composable
+fun TopBooster(isSwipe: Boolean) {
+    Log.e("nicolas", "TopBooster recomposition")
+    var rotate: Float by remember { mutableFloatStateOf(0f) }
+
+    if (isSwipe) {
+        LaunchedEffect(Unit) {
+            while (rotate < 10f) {
+                rotate += if (rotate < 0.5f) 0.3f else 0.5f
+                delay(10)
+            }
+        }
+    }
+
+    Column(modifier = Modifier.rotate(rotate)) {
+        Image(
+            painter = painterResource(R.drawable.piece_of_boosters),
+            contentDescription = "Piece in aluminium of booster",
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        SecondQuadrilateral(modifier = Modifier.padding(horizontal = 4.dp))
     }
 }
 
@@ -61,11 +143,21 @@ fun Booster(height: Int = 350, width: Int = 200) {
             modifier = Modifier
                 .width(width.dp),
         ) {
+            var rotate: Float by remember { mutableFloatStateOf(0f) }
+            if (isSwipe) {
+                LaunchedEffect(Unit) {
+                    while (rotate < 10f) {
+                        rotate += if (rotate < 0.5f) 0.3f else 0.5f
+                        delay(10)
+                    }
+                }
+            }
             Image(
                 painter = painterResource(R.drawable.piece_of_boosters),
                 contentDescription = "Piece in aluminium of booster",
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .rotate(rotate),
                 contentScale = ContentScale.FillWidth
             )
             Box(
@@ -74,7 +166,6 @@ fun Booster(height: Int = 350, width: Int = 200) {
                     .padding(horizontal = 2.dp)
                     .fillMaxWidth(),
             ) {
-                val rotate = if (isSwipe) 90f else 0f
                 SecondQuadrilateral(modifier = Modifier.rotate(rotate))
                 Quadrilateral()
             }
@@ -92,11 +183,8 @@ fun Booster(height: Int = 350, width: Int = 200) {
 
 
 @Composable
-fun Quadrilateral() {
-    Canvas(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
+fun Quadrilateral(modifier: Modifier = Modifier) {
+    Canvas(modifier = modifier) {
         val path = Path().apply {
             moveTo(0f, size.height)
             lineTo(size.width, size.height)
@@ -113,7 +201,7 @@ fun Quadrilateral() {
 }
 
 @Composable
-fun SecondQuadrilateral(modifier: Modifier) {
+fun SecondQuadrilateral(modifier: Modifier = Modifier) {
     Canvas(
         modifier = modifier
             .fillMaxSize()
@@ -132,31 +220,3 @@ fun SecondQuadrilateral(modifier: Modifier) {
         )
     }
 }
-
-@Preview
-@Composable
-fun PreviewQuadrilateral() {
-    Quadrilateral()
-}
-
-//@Preview
-//@Composable
-//fun SliderMinimalExample() {
-//    var sliderPosition = remember { mutableFloatStateOf(0f) }
-//    var isDragging = remember { mutableStateOf(false) }
-//
-//    Box(
-//
-//    ) {
-//        Log.e("nicolas", "SliderMinimalExample - : ${isDragging.value}")
-//        Slider(
-//            value = sliderPosition.floatValue,
-//            onValueChange = {
-//                if (isDragging.value) {  // Seul un clic suivi d'un glissement permet de changer la valeur
-//                    sliderPosition.floatValue = it
-//                }
-//            },
-//            modifier = Modifier.fillMaxWidth()
-//        )
-//    }
-//}
