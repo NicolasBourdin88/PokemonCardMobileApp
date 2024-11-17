b3aed9328aede75b68fcadbffbfab354e17e3c21
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2024-11-17 22:11:33 +0100

    refractor: Remove useless comment

--- app/src/main/java/com/example/pokemonultimate/data/api/ApiResponse.kt
+++ app/src/main/java/com/example/pokemonultimate/data/api/ApiResponse.kt
@@ -9,7 +9,7 @@ open class ApiResponse<T>(
     val data: @RawValue T? = null,
     val error: ApiError? = null,
     var translatedError: Int = 0,
-    val page: Int = 0, // page actuel
-    val count: Int = 0, // nombre de carte sur la page
-    val totalCount: Int = -1, // nombre total de cartes possible sur toutes les pages
+    val page: Int = 0,
+    val count: Int = 0,
+    val totalCount: Int = -1,
 )
