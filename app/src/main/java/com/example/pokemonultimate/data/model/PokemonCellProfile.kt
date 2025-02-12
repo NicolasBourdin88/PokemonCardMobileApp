13bf99ed020b373af6961c294dc842bd357c8608
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2025-02-12 09:33:11 +0100

    feat: Display random booster set

--- app/src/main/java/com/example/pokemonultimate/data/model/PokemonCellProfile.kt
+++ app/src/main/java/com/example/pokemonultimate/data/model/PokemonCellProfile.kt
@@ -3,8 +3,18 @@ package com.example.pokemonultimate.data.model
 import androidx.annotation.DrawableRes
 import androidx.compose.ui.graphics.Brush
 import com.example.pokemonultimate.R
-import com.example.pokemonultimate.ui.theme.*
-
+import com.example.pokemonultimate.ui.theme.cardFireFirstColor
+import com.example.pokemonultimate.ui.theme.cardFireSecondColor
+import com.example.pokemonultimate.ui.theme.cardGrassFirstColor
+import com.example.pokemonultimate.ui.theme.cardGrassSecondColor
+import com.example.pokemonultimate.ui.theme.cardIceFirstColor
+import com.example.pokemonultimate.ui.theme.cardIceSecondColor
+import com.example.pokemonultimate.ui.theme.cardLightningFirstColor
+import com.example.pokemonultimate.ui.theme.cardLightningSecondColor
+import com.example.pokemonultimate.ui.theme.cardNormalFirstColor
+import com.example.pokemonultimate.ui.theme.cardNormalSecondColor
+import com.example.pokemonultimate.ui.theme.cardWaterFirstColor
+import com.example.pokemonultimate.ui.theme.cardWaterSecondColor
 
 enum class PokemonCellProfile(
     @DrawableRes val pokemonCellImage: Int,
@@ -13,28 +23,23 @@ enum class PokemonCellProfile(
     ICE(
         pokemonCellImage = R.drawable.image_card_ice,
         brush = Brush.linearGradient(listOf(cardIceFirstColor, cardIceSecondColor)),
-
-        ),
+    ),
     FIRE(
         pokemonCellImage = R.drawable.image_card_fire,
         brush = Brush.linearGradient(listOf(cardFireFirstColor, cardFireSecondColor)),
-
-        ),
+    ),
     PLANT(
         pokemonCellImage = R.drawable.image_card_grass,
         brush = Brush.linearGradient(listOf(cardGrassFirstColor, cardGrassSecondColor)),
-
-        ),
-    ELECTRIC(
+    ),
+    LIGHTNING(
         pokemonCellImage = R.drawable.image_card_lightning,
         brush = Brush.linearGradient(listOf(cardLightningFirstColor, cardLightningSecondColor)),
-
-        ),
+    ),
     WATER(
         pokemonCellImage = R.drawable.image_card_water,
         brush = Brush.linearGradient(listOf(cardWaterFirstColor, cardWaterSecondColor)),
-
-        ),
+    ),
     NORMAL(
         pokemonCellImage = R.drawable.image_card_normal,
         brush = Brush.linearGradient(listOf(cardNormalFirstColor, cardNormalSecondColor)),
